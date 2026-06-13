338c120fedb2aef28881d062a31d3fd011fccf3b
Teru-Terun <hack90x@gmail.com>
2026-06-13 19:59:28 +0700

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://127.0.0.1:8000';
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
